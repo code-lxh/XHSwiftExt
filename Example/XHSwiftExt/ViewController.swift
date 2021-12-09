e81139092b690327927a42be45421eb6f992e4ef
lxh <xhli@ibingli.cn>
2021-12-09 17:00:39 +0800

    Initial commit

---
+++ Example/XHSwiftExt/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  XHSwiftExt
+//
+//  Created by code-lxh on 12/09/2021.
+//  Copyright (c) 2021 code-lxh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
