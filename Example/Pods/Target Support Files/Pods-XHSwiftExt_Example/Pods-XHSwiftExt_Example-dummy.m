79ce7901c7e5f2b9c91adf8066a6d3b808a296b5
lxh <xhli@ibingli.cn>
2021-12-09 17:24:41 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-XHSwiftExt_Example/Pods-XHSwiftExt_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XHSwiftExt_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XHSwiftExt_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-XHSwiftExt_Tests/Pods-XHSwiftExt_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XHSwiftExt_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XHSwiftExt_Tests
+@end
